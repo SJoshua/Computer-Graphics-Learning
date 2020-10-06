3104595ffd0c9b6f2a785f6dc2c6ea8ccbc2e633
SJoshua <JoshuaSRKF@gmail.com>
2020-10-06 21:25:47 +0800

    add(exp-1): walk animation

--- experiment-1/character_model.js
+++ experiment-1/character_model.js
@@ -4,6 +4,26 @@ import * as THREE from 'https://threejs.org/build/three.module.js';
 // use jsm instead of js - they were deprecated.
 import { OrbitControls } from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
 import { VRMLoader } from 'https://threejs.org/examples/jsm/loaders/VRMLoader.js';
+import { GLTFLoader } from 'https://threejs.org/examples/jsm/loaders/GLTFLoader.js';
+
+// glb : binary format
+// gltf: json format
+// vrm : extended-gltf
+
+function translate(clip, bones) {
+    var arr = [];
+    for (var i = 0; i < 1; i++) {
+        arr.push(bones[i].node);
+    }
+    arr.sort();
+    var new_tracks = [];
+    for (var i = 0; i < 1; i++) {
+        for (var j = 0; j < 3; j++) {
+            new_tracks.push(clip.tracks[i * 3 + j]);
+        }
+    }
+    return new_tracks;
+}
 
 function main() {
     const canvas = document.querySelector('#c');
@@ -93,27 +113,136 @@ function main() {
         camera.lookAt(boxCenter.x, boxCenter.y, boxCenter.z);
     }
 
-    // load character model - vrm (extended-glTF)
+    // load character model
     {
+        var mixer;
+        var clock = new THREE.Clock();
+
+        const gltfLoader = new GLTFLoader();
         const vrmLoader = new VRMLoader();
-        vrmLoader.load('character.vrm', (vrm) => {
-            const root = vrm.scene;
-            scene.add(root);
+        
+        gltfLoader.load('char_with_motion.gltf', (gltf) => {
+            console.log(gltf);
+            var animation_walk = gltf.animations[0];
+            animation_walk.tracks = animation_walk.tracks.filter(x =>
+                [
+                    "Hips",
+                    // "Spine",
+                    // "Chest",
+                    // "Neck",
+                    // "Head",
+                    // "RightEye",
+                    // "LeftEye",
+                    // "breastR",
+                    // "ShoulderR",
+                    "Upper_ArmR",
+                    "Lower_ArmR",
+                    // "HandR",
+                    // "Little_ProximalR",
+                    // "Little_IntermediateR",
+                    // "Little_DistalR",
+                    // "Ring_ProximalR",
+                    // "Ring_IntermediateR",
+                    // "Ring_DistalR",
+                    // "Thumb_ProximalR",
+                    // "Thumb_IntermediateR",
+                    // "Thumb_DistalR",
+                    // "Index_ProximalR",
+                    // "Index_IntermediateR",
+                    // "Index_DistalR",
+                    // "Middle_ProximalR",
+                    // "Middle_IntermediateR",
+                    // "Middle_DistalR",
+                    // "breastL",
+                    // "ShoulderL",
+                    "Upper_ArmL",
+                    "Lower_ArmL",
+                    // "HandL",
+                    // "Index_ProximalL",
+                    // "Index_IntermediateL",
+                    // "Index_DistalL",
+                    // "Ring_ProximalL",
+                    // "Ring_IntermediateL",
+                    // "Ring_DistalL",
+                    // "Little_ProximalL",
+                    // "Little_IntermediateL",
+                    // "Little_DistalL",
+                    // "Thumb_ProximalL",
+                    // "Thumb_IntermediateL",
+                    // "Thumb_DistalL",
+                    // "Middle_ProximalL",
+                    // "Middle_IntermediateL",
+                    // "Middle_DistalL",
+                    "Upper_LegR",
+                    "Lower_LegR",
+                    // "FootR",
+                    // "ToesR",
+                    "Upper_LegL",
+                    "Lower_LegL",
+                    // "FootL",
+                    // "ToesL"
+                ]
+                    .indexOf(x.name.split(".")[0]) != -1)
+                    .filter(x => (x.name.split(".")[1] == "quaternion" || x.name == "Hips.position"))
 
-            // compute the box that contains all the stuff
-            // from root and below
-            const box = new THREE.Box3().setFromObject(root);
+            animation_walk.tracks.forEach(x => {
+                if (x.name == "Hips.quaternion") {
+                    for (let i = 0; i < x.times.length; i++) {
+                        let a = x.values[i * 4 + 0]
+                        let b = x.values[i * 4 + 1]
+                        let c = x.values[i * 4 + 2]
+                        let d = x.values[i * 4 + 3]
+                        //bd-ac, bd-ca, db-ac, db-ca
+                        x.values[i * 4 + 0] = b
+                        x.values[i * 4 + 1] = a
+                        x.values[i * 4 + 2] = -d
+                        x.values[i * 4 + 3] = c
+                    }
+                } else if (x.name == "FootL.quaternion" || x.name == "FootR.quaternion" ) {
+                    
+                } else if (x.name != "Hips.position") {
+                    for (let i = 0; i < x.times.length; i++) {
+                        let a = x.values[i * 4 + 0]
+                        let b = x.values[i * 4 + 1]
+                        let c = x.values[i * 4 + 2]
+                        let d = x.values[i * 4 + 3]
+                        x.values[i * 4 + 0] = c
+                        x.values[i * 4 + 1] = b
+                        x.values[i * 4 + 2] = a
+                        x.values[i * 4 + 3] = -d
+                    }
+                }
+            })
 
-            const boxSize = box.getSize(new THREE.Vector3()).length();
-            const boxCenter = box.getCenter(new THREE.Vector3());
-
-            // set the camera to frame the box
-            frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
-
-            // update the Trackball controls to handle the new size
-            controls.maxDistance = boxSize * 10;
-            controls.target.copy(boxCenter);
-            controls.update();
+            vrmLoader.load('character.vrm', (vrm) => {
+                console.log(vrm);
+                const root = vrm.scene;
+    
+                mixer = new THREE.AnimationMixer(root);
+                // var bones = vrm.userData.gltfExtensions.VRM.humanoid.humanBones;
+                // animation_walk.tracks = translate(animation_walk, bones);
+                console.log(animation_walk);
+                
+                var action = mixer.clipAction(animation_walk);
+                action.play();
+    
+                scene.add(root);
+    
+                // compute the box that contains all the stuff
+                // from root and below
+                const box = new THREE.Box3().setFromObject(root);
+    
+                const boxSize = box.getSize(new THREE.Vector3()).length();
+                const boxCenter = box.getCenter(new THREE.Vector3());
+    
+                // set the camera to frame the box
+                frameArea(boxSize * 0.5, boxSize, boxCenter, camera);
+    
+                // update the Trackball controls to handle the new size
+                controls.maxDistance = boxSize * 10;
+                controls.target.copy(boxCenter);
+                controls.update();
+            });
         });
     }
 
@@ -136,6 +265,8 @@ function main() {
         }
 
         renderer.render(scene, camera);
+
+        if (mixer) mixer.update(clock.getDelta());
 
         requestAnimationFrame(render);
     }
--- experiment-1/index.md
+++ experiment-1/index.md
@@ -1,3 +1,6 @@
 # Experiment 1
 * [Character Model](character_model.html)
-> To-do: Program with Shader
+> VRM model + glTF motion data (AnimationChip) with some ugly fixes. 
+>> How to export model to glTF without material lost? There are some great models(.vrm).
+>> 
+>> How to make .vrm models animations? They don't contain animation data. The bones seem to be different with .glTF.
